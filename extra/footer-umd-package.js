70bcd40a74dacc525503afab7740fc9f6035bdaa
Philip Lawall <philiplawall@gmail.com>
2023-04-15 22:36:28 +0200

    update

--- extra/footer-umd-package.js
+++
@@ -1,7 +0,0 @@
-/* extra/footer-umd-package.js */
-
-// generated code by WebPack only for umd distributions
-window['bridgeReceiveIntegerFromNative'] = CrComLib.bridgeReceiveIntegerFromNative;
-window['bridgeReceiveBooleanFromNative'] = CrComLib.bridgeReceiveBooleanFromNative;
-window['bridgeReceiveStringFromNative'] = CrComLib.bridgeReceiveStringFromNative;
-window['bridgeReceiveObjectFromNative'] = CrComLib.bridgeReceiveObjectFromNative;
